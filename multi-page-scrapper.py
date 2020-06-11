609da5e4a667fe645cb152cddc1e4a54ff495cab
e-ian <ianemma70@gmail.com>
2020-06-11 23:35:33 +0300

    script for single page

--- multi-page-scrapper.py
+++ multi-page-scrapper.py
@@ -30,3 +30,30 @@ print(first_mscore)
 #first movie votes
 first_votes = int(first_movie.find('span', attrs = {'name':'nv'})['data-value'])
 print(first_votes)
+
+# Lists to store the scraped data
+names = []
+years = []
+imdb_ratings = []
+metascores = []
+votes = []
+
+# Extract data from individual movie container
+for container in movie_containers:
+# If the metascore is not none, then extract
+    if container.find('div', class_='ratings-metascore') is not None:
+        name = container.h3.a.text
+        names.append(name)
+
+        year = container.h3.find('span', class_='lister-item-year').text
+        years.append(year)
+
+        imdb = float(container.strong.text)
+        imdb_ratings.append(imdb)
+
+        m_score = container.find('span', class_='metascore').text
+        metascores.append(int(m_score))
+
+        vote = container.find('span', attrs = {'name':'nv'})['data-value']
+        votes.append(int(vote))
+
