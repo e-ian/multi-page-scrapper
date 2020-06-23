e5b9f921f8d12f15f3f4eacc2b5bbebc8f0364e5
e-ian <ianemma70@gmail.com>
2020-06-23 19:54:36 +0300

    remove comments

--- multi-page-scrapper.py
+++ multi-page-scrapper.py
@@ -5,7 +5,6 @@ import pandas as pd
 url = 'http://www.imdb.com/search/title?release_date=2017&sort=num_votes,desc&page=1'
 
 response = get(url)
-# print(response.text[:500])
 
 html_soup = BeautifulSoup(response.text, 'html.parser')
 
